f171700b02da1ee4595fc3924ed2419b011e0888
Ivanyan0516 <Ivanyan0516@gmail.com>
2021-12-12 22:10:09 +0800

    update files

---
+++ Resnet18/main_single_gpu.py
@@ -0,0 +1,87 @@
+import paddle
+import paddle.nn as nn
+from resnet import ResNet18
+from dataset import get_dataset
+from dataset import get_dataloader
+from utils import AverageMeter
+
+def train_one_epoch(model, dataloader, criterion, optimizer, epoch, total_epoch, report_freq=10):
+    model.train()
+    loss_meter = AverageMeter()
+    acc_meter = AverageMeter()
+
+    for batch_id, data in enumerate(dataloader):
+        image = data[0]
+        label = data[1]
+
+        out = model(image) #inference
+        loss = criterion(out, label)
+
+        loss.backward()
+        optimizer.step()
+        optimizer.clear_grad()
+
+        pred = nn.functional.softmax(out, axis=1)
+        acc = paddle.metric.accuracy(pred, label.unsqueeze(-1))
+
+        batch_size = image.shape[0]
+        loss_meter.update(loss.cpu().numpy()[0], batch_size)
+        acc_meter.update(acc.cpu().numpy()[0], batch_size)
+        if batch_id > 0 and batch_id % report_freq == 0:
+            print(f'----- Batch[{batch_id}/{len(dataloader)}], Loss: {loss_meter.avg:.4}, Acc@1: {acc_meter.avg:.2}')
+        # if batch_id > 0:
+        #     print(f'------ Batch {batch_id}, loss={loss_meter.avg}, acc={acc_meter.avg}')
+    print(f'----- Epoch[{epoch}/{total_epoch}], Loss: {loss_meter.avg:.4}, Acc@1: {acc_meter.avg:.2}')
+
+def validation(model, dataloader, criterion, report_freq=10):
+    model.eval()
+    loss_meter = AverageMeter()
+    acc_meter = AverageMeter()
+
+    for batch_id, data in enumerate(dataloader):
+        image = data[0]
+        label = data[1]
+
+        out = model(image)  # inference
+        loss = criterion(out, label)
+
+        pred = nn.functional.softmax(out, axis=1)
+        acc = paddle.metric.accuracy(pred, label.unsqueeze(-1))
+
+        batch_size = image.shape[0]
+        loss_meter.update(loss.cpu().numpy()[0], batch_size)
+        acc_meter.update(acc.cpu().numpy()[0], batch_size)
+        # if batch_id > 0:
+        #     print(f'------ Batch {batch_id}, loss={loss_meter.avg}, acc={acc_meter.avg}')
+        if batch_id > 0 and batch_id % report_freq == 0:
+            print(f'----- Batch[{batch_id}/{len(dataloader)}], Loss: {loss_meter.avg:.4}, Acc@1: {acc_meter.avg:.2}')
+    print(f'----- Validation Loss: {loss_meter.avg:.4}, Acc@1: {acc_meter.avg:.2}')
+
+def main():
+    total_epoch = 20
+    batch_size = 16
+
+    model = ResNet18(num_classes=10)
+
+    train_dataset = get_dataset(mode='train')
+    train_dataloader = get_dataloader(train_dataset, mode='train', batch_size=batch_size)
+    val_dataset = get_dataset(mode='test')
+    val_dataloader = get_dataloader(val_dataset, mode='test', batch_size=batch_size)
+
+    criterion = nn.CrossEntropyLoss()
+    scheduler = paddle.optimizer.lr.CosineAnnealingDecay(0.02, total_epoch=total_epoch)
+
+    optimizer = paddle.optimizer.Momentum(learning_rate=scheduler,
+                                          parameters=model.parameters(),
+                                          momentum=0.9,
+                                          weight_decay=5e-4)
+
+    for epoch in range(1, total_epoch+1):
+        train_one_epoch(model, train_dataloader, criterion, optimizer, epoch, total_epoch)
+        scheduler.step()
+        validation(model, val_dataloader, criterion)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ Resnet18/resnet.py
@@ -0,0 +1,107 @@
+from PIL import Image
+import paddle
+import paddle.nn as nn
+
+paddle.set_device('cpu')
+
+class Identity(nn.Layer):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return x
+
+class Block(nn.Layer):
+    def __init__(self, in_dim, out_dim, stride):
+        super().__init__()
+        self.conv1 = nn.Conv2D(in_dim, out_dim, kernel_size=3, stride=stride, padding=1, bias_attr=False)
+        self.bn1 = nn.BatchNorm2D(out_dim)
+        self.conv2 = nn.Conv2D(out_dim, out_dim, kernel_size=3, stride=1, padding=1, bias_attr=False)
+        self.bn2 = nn.BatchNorm2D(out_dim)
+        self.relu = nn.ReLU()
+
+        if stride == 2 or in_dim != out_dim:
+            self.downsample = nn.Sequential(*[
+                nn.Conv2D(in_dim, out_dim, 1, stride=stride),
+                nn.BatchNorm2D(out_dim)])
+        else:
+            self.downsample = Identity()
+
+    def forward(self, x):
+        h = x
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        identity = self.downsample(h)
+        x = x + identity
+        x = self.relu(x)
+        return x
+
+class ResNet18(nn.Layer):
+    def __init__(self, in_dim=64, num_classes=10):
+        super().__init__()
+        self.in_dim = in_dim
+        # stem layers
+        self.conv1 = nn.Conv2D(in_channels=3,
+                               out_channels=in_dim,
+                               kernel_size=3,
+                               stride=1,
+                               padding=1,
+                               bias_attr=False
+                               )
+        self.bn1 = nn.BatchNorm2D(in_dim)
+        self.relu = nn.ReLU()
+
+        # blocks
+        self.layers1 = self._make_layer(dim=64, n_blocks=2, stride=1)
+        self.layers2 = self._make_layer(dim=128, n_blocks=2, stride=2)
+        self.layers3 = self._make_layer(dim=256, n_blocks=2, stride=2)
+        self.layers4 = self._make_layer(dim=512, n_blocks=2, stride=2)
+
+
+
+        # head layer
+        self.avgpool = nn.AdaptiveAvgPool2D(1)
+        self.classifier = nn.Linear(512, num_classes)
+
+    def _make_layer(self, dim, n_blocks, stride):
+        layer_list = []
+        layer_list.append(Block(self.in_dim, dim, stride=stride))
+        self.in_dim = dim
+        for i in range(1, n_blocks):
+            layer_list.append(Block(self.in_dim, dim, stride=1))
+        return nn.Sequential(*layer_list)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        # print('check:', x.shape)  #[4,64,32,32]
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.layers1(x)
+        # print('check:', x.shape)  #[4,64,32,32]
+        x = self.layers2(x)
+        # print('check:', x.shape)  # [4,128,16,16]
+        x = self.layers3(x)
+        x = self.layers4(x)
+        # print('check:', x.shape)  # [4,512,4,4]
+        x = self.avgpool(x)
+        # print('check:', x.shape)  #[4,512,1,1]
+        x = x.flatten(1)
+        # print('check:', x.shape)  # [4,512]
+        x = self.classifier(x)
+        return x
+
+def main():
+    # 1. Create a Tensor
+    t = paddle.randn([4, 3, 32, 32])
+    # print(t)
+    model = ResNet18()
+    out = model(t)
+    print(out.shape)
+    # print(out)
+    # print(model)
+
+if __name__ == '__main__':
+    main()
